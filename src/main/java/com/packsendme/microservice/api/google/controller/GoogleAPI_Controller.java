5341328338cdba0e180c992978081ff773951b73
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-19 20:07:47 +0100

    NewVersion-Release/0.2.0-V0010

--- src/main/java/com/packsendme/microservice/api/google/controller/GoogleAPI_Controller.java
+++ src/main/java/com/packsendme/microservice/api/google/controller/GoogleAPI_Controller.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class GoogleAPI_Controller {
 		return googleDirection.loadDistancesCities(origins, destinations);
 	}
 
-	@GetMapping("/tolls")
+	@PostMapping("/tolls")
 	public ResponseEntity<?> getTolls(
 			@Validated @RequestBody SimulationRequest_Dto simulation) throws JsonProcessingException, IOException {		
 		return tolls_Service.getTollsAnalyze(simulation);
