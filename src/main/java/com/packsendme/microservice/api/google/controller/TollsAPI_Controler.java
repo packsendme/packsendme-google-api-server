dd0eac009d0c3f76f9791fa5166e5180380f1b07
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-20 12:41:48 +0100

    NewVersion-Release/0.2.0-V01

--- src/main/java/com/packsendme/microservice/api/google/controller/TollsAPI_Controler.java
+++ src/main/java/com/packsendme/microservice/api/google/controller/TollsAPI_Controler.java
@@ -1,7 +1,9 @@
 package com.packsendme.microservice.api.google.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TollsAPI_Controler {
 
 	@GetMapping("/Ricardo")
